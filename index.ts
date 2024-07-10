7234fc7feccb100b6b335ab2ba30981910396eb3
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-07-10 17:23:13 +0200

    setup index.html and running server

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response, Application } from "express";
+import path from 'path';
+
+const app: Application = express();
+const port = 3000;
+const homePath = path.join(__dirname, 'index.html');
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(homePath);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
+
+
