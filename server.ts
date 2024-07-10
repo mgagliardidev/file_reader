301ab19e5902ef173d6edc901d9646496ebca5e5
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-07-10 17:49:34 +0200

    adjust files

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const app: Application = express();
 const port = 3000;
-const homePath = path.join(__dirname, 'index.html');
+const homePath = path.join(__dirname, '/view/index.html');
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(homePath);
